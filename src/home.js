97af90774c410646d7d9330f46a0acaed278d8bc
Joe <willievaughn1004@gmail.com>
2023-09-07 23:09:11 -0400

    Add main and change modules to add content to main

--- src/home.js
+++ src/home.js
@@ -1,39 +1,7 @@
 import { buildComponent, appendComponent } from "./components.js";
 
-function createHeader() {
-  const headerComponent = buildComponent("header", "");
-
-  const headerText = buildComponent("div", "Joe's Pizzaria", {
-    class: "headerText",
-  });
-  const headerSubText = buildComponent("div", "(We're not a cult)", {
-    class: "headerSubText",
-  });
-  const links = createNav();
-
-  headerComponent.appendChild(headerText);
-  headerComponent.appendChild(headerSubText);
-  headerComponent.appendChild(links);
-
-  appendComponent("#content", headerComponent);
-}
-
-function createNav() {
-  const navComponent = buildComponent("nav", "");
-
-  const link1 = buildComponent("button", "Home");
-  const link2 = buildComponent("button", "Menu");
-  const link3 = buildComponent("button", "Contact");
-
-  navComponent.appendChild(link1);
-  navComponent.appendChild(link2);
-  navComponent.appendChild(link3);
-
-  return navComponent;
-}
-
-function createCopy() {
-  const copyComponent = buildComponent("div", "", { class: "copy" });
+function createHomeCopy(elem) {
+  const copyHomeComponent = buildComponent("div", "", { class: "copy" });
 
   const paragraph1 = buildComponent("p", "It started as an obsession");
   const paragraph2 = buildComponent("p", "But became my guiding light");
@@ -43,38 +11,12 @@ function createCopy() {
   });
   const paragraph3 = buildComponent("p", "Order online, visit us, or join us!");
 
-  copyComponent.appendChild(paragraph1);
-  copyComponent.appendChild(paragraph2);
-  copyComponent.appendChild(image);
-  copyComponent.appendChild(paragraph3);
+  copyHomeComponent.appendChild(paragraph1);
+  copyHomeComponent.appendChild(paragraph2);
+  copyHomeComponent.appendChild(image);
+  copyHomeComponent.appendChild(paragraph3);
 
-  appendComponent("#content", copyComponent);
+  appendComponent(`${elem}`, copyHomeComponent);
 }
 
-function createFooter() {
-  const footerComponent = buildComponent("footer", "");
-
-  const copyrightText = buildComponent("div", "Copyright");
-  const githubUsername = buildComponent("div", "willievaughn1004");
-  const copyrightSymbol = buildComponent("i", "", {
-    class: "fa-regular fa-copyright",
-  });
-  const githubSymbol = buildComponent("i", "", {
-    class: "fa-brands fa-github",
-  });
-
-  footerComponent.appendChild(copyrightText);
-  footerComponent.appendChild(copyrightSymbol);
-  footerComponent.appendChild(githubUsername);
-  footerComponent.appendChild(githubSymbol);
-
-  appendComponent("#content", footerComponent);
-}
-
-const createHomePage = () => {
-  createHeader();
-  createCopy();
-  createFooter();
-};
-
-export default createHomePage;
+export default createHomeCopy;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
-import createHomePage from "./home.js"
+import { buildComponent, appendComponent } from "./components.js";
+import createHomeCopy from "./home.js";
 
-createHomePage();
+function createHeader() {
+  const headerComponent = buildComponent("header", "");
+
+  const headerText = buildComponent("div", "Joe's Pizzaria", {
+    class: "headerText",
+  });
+  const headerSubText = buildComponent("div", "(We're not a cult)", {
+    class: "headerSubText",
+  });
+  const links = createNav();
+
+  headerComponent.appendChild(headerText);
+  headerComponent.appendChild(headerSubText);
+  headerComponent.appendChild(links);
+
+  function createNav() {
+    const navComponent = buildComponent("nav", "");
+
+    const link1 = buildComponent("button", "Home", { class: "home" });
+    const link2 = buildComponent("button", "Menu", { class: "menu" });
+    const link3 = buildComponent("button", "Contact", { class: "contact" });
+
+    navComponent.appendChild(link1);
+    navComponent.appendChild(link2);
+    navComponent.appendChild(link3);
+
+    return navComponent;
+  }
+
+  appendComponent("#content", headerComponent);
+}
+
+function createMain() {
+  const mainComponent = buildComponent("main", "", { class: "main"});
+
+  appendComponent("#content", mainComponent);
+}
+
+function createFooter() {
+  const footerComponent = buildComponent("footer", "");
+
+  const copyrightText = buildComponent("div", "Copyright");
+  const githubUsername = buildComponent("div", "willievaughn1004");
+  const copyrightSymbol = buildComponent("i", "", {
+    class: "fa-regular fa-copyright",
+  });
+  const githubSymbol = buildComponent("i", "", {
+    class: "fa-brands fa-github",
+  });
+
+  footerComponent.appendChild(copyrightText);
+  footerComponent.appendChild(copyrightSymbol);
+  footerComponent.appendChild(githubUsername);
+  footerComponent.appendChild(githubSymbol);
+
+  appendComponent("#content", footerComponent);
+}
+
+function addPageContent() {
+  createHeader();
+  createMain();
+
+
+  // Dom Elements
+  const mainContent = document.querySelector(".main")
+  const homeButton = document.querySelector(".home");
+  const menuButton = document.querySelector(".menu");
+  const contactButton = document.querySelector(".contact");
+
+  menuButton.addEventListener("click", function () {
+    mainContent.innerHTML = "";
+  });
+
+  createHomeCopy("main");
+  createFooter();
+}
+
+addPageContent();
